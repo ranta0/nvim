f9aa700e3906090371ea4466abacf044e0ce508c
Riccardo Esposito <ric.espo9@gmail.com>
2024-02-27 09:25:29 +0100

    Fix: lint breaking if linters not installed

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -8,8 +8,8 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      markdown = { "markdownlint" },
-      php = { "phpstan" },
+      -- markdown = { "markdownlint" },
+      -- php = { "phpstan" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
