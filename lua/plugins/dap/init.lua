54bac128506c825c4ebbf3b46d9a52f49c99ca1b
Riccardo Esposito <ric.espo9@gmail.com>
2024-03-20 14:29:37 +0100

    Added new dap-ui required import

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -6,6 +6,7 @@ return {
     "rcarriga/nvim-dap-ui",
     "williamboman/mason.nvim",
     "jay-babu/mason-nvim-dap.nvim",
+    "nvim-neotest/nvim-nio",
   },
   config = function()
     local dap = require("dap")
