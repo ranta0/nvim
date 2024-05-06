9b06b17e16e2904592caeffc7c91ecd2a1036363
Riccardo Esposito <ric.espo9@gmail.com>
2024-05-06 09:21:11 +0200

    feat: added linting

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,28 @@
+local utils = require("utils")
+
+return {
+  "mfussenegger/nvim-lint",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile", "SessionLoadPost" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      -- markdown = { "markdownlint" },
+      -- php = { "phpstan" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>ml", function()
+      lint.try_lint()
+    end, utils.remap_opt("[m]ode [l]inting for current file", true))
+  end,
+}
