b9dd434cc5f849b4e98574841f880bb5a64319d0
Riccardo Esposito <ric.espo9@gmail.com>
2024-05-11 15:19:15 +0200

    Feat: bash-fc autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "qf" },
   command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = { "bash-fc.*" },
+  command = [[set syntax=sh filetype=sh]],
+})
